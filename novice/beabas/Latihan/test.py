4430a35759da596fc1bfb7d53bc0f46214fdac15
septiannurtrir <septiannurtrir@gmail.com>
2019-09-04 03:06:41 -0700

    baru

--- novice/beabas/Latihan/test.py
+++
@@ -1,6 +0,0 @@
-import mysql.connector
-
-cnx = mysql.connector.connect(user='septiannurtrir', password='sandaljebatds07',
-                              host='127.0.0.1',
-                              database='siswa') #jejeDB dibuat manual, tapi ngga dilihatin di dokumentasi ini
-cnx.close()
